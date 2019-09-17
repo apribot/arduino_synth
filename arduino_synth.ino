9bbb7728d5336c2ff53290d81a0e04d0e02fdbb2
mint <apribot@gmail.com>
2019-09-16 22:43:57 -0400

    first stab at implementing ADSR, some cleanup

--- arduino_synth.ino
+++ arduino_synth.ino
@@ -1,8 +1,5 @@
 /* TODO:
- * don't care about lfo
- * change pins/pots to be full ADSR maybe?
- * A        D      S       R
- * waveform cutoff reso
+    idk
  */
 
 #include <MozziGuts.h>
@@ -24,13 +21,18 @@ const int8_t *WAVE_TABLES[NUM_TABLES] = {SQUARE_ANALOGUE512_DATA, SIN512_DATA, S
 MIDI_CREATE_DEFAULT_INSTANCE();
 #define CONTROL_RATE 64
 
+/*
+ *  current pot to input mapping
+ *  4 5 6 7
+ *  0 2 3
+ */
+#define ATTACK_POT 4
+#define DECAY_POT 5
+#define SUSTAIN_POT 6
+#define RELEASE_POT 7
 #define WAVEFORM_POT 0
-#define ATTACK_POT 2
-#define DECAY_POT 3
-#define LP_CUTOFF_POT 4
-#define LP_RESO_POT 5
-#define LFO_SPEED_POT 6
-#define LFO_WAVEFORM_POT 7
+#define LP_RESO_POT 2
+#define LP_CUTOFF_POT 3
 
 Oscil <TABLE_SIZE, AUDIO_RATE> aOscil(SQUARE_ANALOGUE512_DATA);
 
@@ -40,34 +42,29 @@ boolean note_is_on = false;
 
 byte note;
 
-/*
-* Paramethers of note
-*/
 //Intensity of phase
 byte attack_level = 255;
 byte decay_level = 0;
 
 //Duration of each phase
-unsigned int attack=50;
-unsigned int decay=10;
-unsigned int sustain=20;
-unsigned int release_ms=20;
+unsigned int sustain=30000; // arbitrarily long, idk
 
+//enum for nice names
 enum Potentiometers {
   WaveFormPot,
   AttackPot,
   DecayPot,
+  SustainPot,
+  ReleasePot,
   LPFCutoffPot,
   LPFResonancePot,
-  LFOSpeedPot,
-  LFOWaveFormPot,
   POTENTIOMETER_COUNT
 };
 
 uint16_t pots[POTENTIOMETER_COUNT];
 
 //Used to mute output if no notes are being played.
-boolean no_nota=true;
+boolean no_note=true;
 EventDelay noteDelay;
 
 const int8_t *potValueToWaveTable (unsigned int value) {
@@ -82,55 +79,56 @@ void readPots() {
   pots[WaveFormPot] = mozziAnalogRead(WAVEFORM_POT);
   pots[AttackPot] = mozziAnalogRead(ATTACK_POT);
   pots[DecayPot] = mozziAnalogRead(DECAY_POT);
+  pots[SustainPot] = mozziAnalogRead(SUSTAIN_POT);
+  pots[ReleasePot] = mozziAnalogRead(RELEASE_POT);  
   pots[LPFCutoffPot] = mozziAnalogRead(LP_CUTOFF_POT);
   pots[LPFResonancePot] = mozziAnalogRead(LP_RESO_POT);
-  pots[LFOSpeedPot] = mozziAnalogRead(LFO_SPEED_POT);
-  pots[LFOWaveFormPot] = mozziAnalogRead(LFO_WAVEFORM_POT);  
 }
 
-void handleNoteOn(byte channel, byte pitch, byte velocity)
-{
-
+void grabSettings() {
     aOscil.setTable(potValueToWaveTable(pots[WaveFormPot]));
 
+    envelope.setLevels(
+      attack_level,
+      decay_level, 
+      pots[SustainPot] >> 2, 
+      0
+    );
+
     envelope.setTimes(
-      pots[AttackPot]+50,
-      (pots[DecayPot] * 5) + 20,
+      ((pots[AttackPot] * pots[AttackPot]) >> 7) + 50, // exponential from 50ms to 8.242s
+      ((pots[DecayPot]  * pots[DecayPot])  >> 7) + 20, // exponential from 20ms to 8.212s
       sustain,
-      release_ms);   
+      pots[ReleasePot] * 5
+    );   
+}
 
-
-    aOscil.setFreq((int)mtof(pitch));
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
+    grabSettings();
     note_is_on = true;
-    
+    aOscil.setFreq((int)mtof(pitch));
     envelope.noteOn();
     envelope.update();
     digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)   
     note = pitch;
 }
 
-void handleNoteOff(byte channel, byte pitch, byte velocity)
-{
+void handleNoteOff(byte channel, byte pitch, byte velocity) {
     digitalWrite(13, LOW);   // turn the LED off
 
-    
     if(note == pitch) {
       envelope.noteOff();  
-      noteDelay.start(release_ms+50);
+      noteDelay.start((pots[ReleasePot] * 5)+50);
       note_is_on = false;
     }
 }
 
-void setup(){
+void setup() {
     pinMode(13, OUTPUT);     
 
     readPots();
 
-    envelope.setLevels(attack_level,decay_level, 0, 0);
- 
-    // generate a random new adsr time parameter value in milliseconds
-    envelope.setTimes(attack,decay,sustain,release_ms);   
-
+    grabSettings();
     byte midi_note = 1;
     aOscil.setFreq((int)mtof(midi_note));
   
@@ -145,26 +143,26 @@ void setup(){
 }
 
 
-void updateControl(){      
+void updateControl() {      
     if(noteDelay.ready() && note_is_on == false)
-      no_nota=true;
+      no_note=true;
     else
-      no_nota=false;
+      no_note=false;
   
     readPots();
   
     envelope.update(); //idk if this is needed
 } 
 
-int updateAudio(){
-    if(no_nota)
+int updateAudio() {
+    if(no_note)
       return 0;
     else {
       return ((long) envelope.next() * aOscil.next())>>8;
     }
 }
 
-void loop(){
+void loop() {
     MIDI.read();
     audioHook();     
 }
